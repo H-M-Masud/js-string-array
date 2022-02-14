fd5197aa2c7f0e7a7d7cd47af7db4cdd660e3590
H-M-Masud <hmmasud163@gmail.com>
2022-02-14 08:47:17 +0600

    added lowercase and uppercase

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+const anthem = 'Amer Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+const userInput = 'blackPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput.toLocaleLowerCase())
+// console.log(savedUserName.toLocaleLowerCase())
+if(userInput.toLocaleLowerCase() == savedUserName.toLocaleLowerCase()){
+    console.log('user exist');
+}
+
+if(userInput.toUpperCase() == savedUserName.toUpperCase()){
+    console.log('user exists')
+}
