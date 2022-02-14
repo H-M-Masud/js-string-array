5c3379f78f91abfa5e82ef795f3d1491e09595a9
H-M-Masud <hmmasud163@gmail.com>
2022-02-14 11:09:52 +0600

    try-catch

---
+++ try-catch.js
@@ -0,0 +1,16 @@
+const myName = 'ami';
+const student = undefined;
+// console.log(student)
+/* for(i = 0; i < 10 i++){
+
+} */
+// name = 'tumi';
+// console.log(name);
+
+try{
+    myName = 'tumi';
+}
+catch(error){
+    console.log('getting error',error)
+}
+console.log(myName)
